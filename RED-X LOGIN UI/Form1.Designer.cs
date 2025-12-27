7b8d1758b04978d4b532d514b34967a6f4fa94bd
Administrator <Administrator@WIN-8R3OEA2LJPD>
2025-12-27 23:43:05 +0800

    Add project files.

---
+++ RED-X LOGIN UI/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace RED_X_LOGIN_UI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.loginBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.passwordBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.usernameBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Animated = true;
+            this.loginBtn.BackColor = System.Drawing.Color.Transparent;
+            this.loginBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.loginBtn.BorderRadius = 3;
+            this.loginBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.loginBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.loginBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.loginBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.loginBtn.FillColor = System.Drawing.Color.Maroon;
+            this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold);
+            this.loginBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.loginBtn.HoverState.BorderColor = System.Drawing.Color.Red;
+            this.loginBtn.HoverState.FillColor = System.Drawing.Color.Red;
+            this.loginBtn.HoverState.ForeColor = System.Drawing.Color.Black;
+            this.loginBtn.IndicateFocus = true;
+            this.loginBtn.Location = new System.Drawing.Point(214, 301);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.ShadowDecoration.Color = System.Drawing.Color.White;
+            this.loginBtn.Size = new System.Drawing.Size(199, 36);
+            this.loginBtn.TabIndex = 19;
+            this.loginBtn.Text = "LOGIN";
+            this.loginBtn.UseTransparentBackground = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Animated = true;
+            this.passwordBox.BackColor = System.Drawing.Color.Transparent;
+            this.passwordBox.BorderColor = System.Drawing.Color.Maroon;
+            this.passwordBox.BorderRadius = 5;
+            this.passwordBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordBox.DefaultText = "";
+            this.passwordBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.passwordBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.passwordBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.passwordBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.passwordBox.FocusedState.BorderColor = System.Drawing.Color.Red;
+            this.passwordBox.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold);
+            this.passwordBox.ForeColor = System.Drawing.Color.White;
+            this.passwordBox.HoverState.BorderColor = System.Drawing.Color.DarkRed;
+            this.passwordBox.Location = new System.Drawing.Point(143, 254);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PlaceholderText = "PASSWORD";
+            this.passwordBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.passwordBox.SelectedText = "";
+            this.passwordBox.ShadowDecoration.Color = System.Drawing.Color.IndianRed;
+            this.passwordBox.Size = new System.Drawing.Size(341, 36);
+            this.passwordBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.passwordBox.TabIndex = 17;
+            this.passwordBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Animated = true;
+            this.usernameBox.BackColor = System.Drawing.Color.Transparent;
+            this.usernameBox.BorderColor = System.Drawing.Color.Maroon;
+            this.usernameBox.BorderRadius = 5;
+            this.usernameBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usernameBox.DefaultText = "";
+            this.usernameBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usernameBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usernameBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.usernameBox.FocusedState.BorderColor = System.Drawing.Color.Red;
+            this.usernameBox.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold);
+            this.usernameBox.ForeColor = System.Drawing.Color.Transparent;
+            this.usernameBox.HoverState.BorderColor = System.Drawing.Color.DarkRed;
+            this.usernameBox.Location = new System.Drawing.Point(143, 210);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.PlaceholderText = "USERNAME";
+            this.usernameBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.usernameBox.SelectedText = "";
+            this.usernameBox.ShadowDecoration.Color = System.Drawing.Color.IndianRed;
+            this.usernameBox.Size = new System.Drawing.Size(341, 36);
+            this.usernameBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.usernameBox.TabIndex = 18;
+            this.usernameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.usernameBox.TextChanged += new System.EventHandler(this.usernameBox_TextChanged);
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(12, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 20;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(627, 388);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button loginBtn;
+        private Guna.UI2.WinForms.Guna2TextBox passwordBox;
+        private Guna.UI2.WinForms.Guna2TextBox usernameBox;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ RED-X LOGIN UI/Form1.cs
@@ -0,0 +1,282 @@
+﻿
+using Guna.UI2.WinForms;
+using LicenseAuth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TheArtOfDevHtmlRenderer.Adapters;
+
+namespace RED_X_LOGIN_UI
+{
+    public partial class LoginForm : Form
+    {
+
+
+        public static api LicenseAuthApp = new api(
+            name: "RED-X LITE",
+            ownerid: "kiFnrOz8gq",
+            secret: "9e83813783c063a0a4a7420a1e0861f949d6be8c2c920f2d9f8b99381ff81d20",
+            version: "1.0"
+        //path: @"Your_Path_Here" // (OPTIONAL) if you use token validation, example: Token.txt or anything.txt and paste token in txt file
+        );
+
+        public LoginForm()
+        {
+
+
+
+            InitializeComponent();
+            LicenseAuthApp.init();
+            // Enable double buffering for smooth particles
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+
+            // Particle timer
+            particleTimer = new Timer();
+            particleTimer.Interval = 30; // ~33 FPS
+            particleTimer.Tick += (s, e) =>
+            {
+                UpdateParticles();
+                this.Invalidate(); // redraw form
+            };
+            particleTimer.Start();
+            this.DoubleBuffered = true;
+
+            // Hide controls initially
+            loginBtn.Visible = false;
+            passwordBox.Visible = false;
+            usernameBox.Visible = false;
+
+
+
+
+
+            // Fade Timer (~60 FPS)
+            fadeTimer = new Timer();
+            fadeTimer.Interval = 16;
+            fadeTimer.Tick += FadeTimer_Tick;
+            fadeTimer.Start();
+
+            // Move Timer (~120 FPS)
+            moveTimer = new Timer();
+            moveTimer.Interval = 8;
+            moveTimer.Tick += MoveTimer_Tick;
+        }
+
+
+
+        // FOR CUSTOM FONT
+        private EmbeddedFontLoader _fontLoader;
+        // Animation positions
+        private PointF startPosition;
+        private PointF targetPosition;
+        private PointF currentPosition;
+
+        // Fade & move
+        private float textOpacity = 0f;
+        private bool fadeInComplete = false;
+        private bool moveComplete = false;
+
+        private Timer fadeTimer;
+        private Timer moveTimer;
+
+        private void FadeTimer_Tick(object sender, EventArgs e)
+        {
+            if (!fadeInComplete)
+            {
+                textOpacity += 0.02f; // smooth fade-in
+                if (textOpacity >= 1f)
+                {
+                    textOpacity = 1f;
+                    fadeInComplete = true;
+                    fadeTimer.Stop();
+                    moveTimer.Start(); // start moving after fade-in
+                }
+                this.Invalidate();
+            }
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (!moveComplete)
+            {
+                float speed = 0.08f;
+                currentPosition.X += (targetPosition.X - currentPosition.X) * speed;
+                currentPosition.Y += (targetPosition.Y - currentPosition.Y) * speed;
+
+                if (Math.Abs(currentPosition.Y - targetPosition.Y) < 1f)
+                {
+                    moveComplete = true;
+
+                    // Hide controls initially
+                    usernameBox.Visible = true;
+                    passwordBox.Visible = true;
+                    loginBtn.Visible = true;
+                }
+
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Clear background first
+            e.Graphics.Clear(Color.Black);
+
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+
+            // Draw particles
+            foreach (var p in particles)
+            {
+                using (SolidBrush b = new SolidBrush(Color.FromArgb((int)p.Life, p.Color)))
+                {
+                    e.Graphics.FillEllipse(b, p.Position.X, p.Position.Y, p.Size, p.Size);
+                }
+            }
+
+            // Draw RED-X LITE text
+            using (Font font = new Font(_fontLoader.GetFontFamilyByName("Nevan RUS"), 28, FontStyle.Regular))
+            using (Brush brush = new SolidBrush(Color.FromArgb((int)(textOpacity * 255), 255, 0, 0)))
+            {
+                e.Graphics.DrawString("RED-X LOGIN UI", font, brush, currentPosition, StringFormat.GenericTypographic);
+            }
+        }
+
+
+        // === Particle System ===
+        private class Particle
+        {
+            public PointF Position;
+            public PointF Velocity;
+            public float Size;
+            public float Life;
+            public Color Color;
+        }
+
+        private readonly List<Particle> particles = new List<Particle>();
+        private readonly Random rand = new Random();
+        private Timer particleTimer;
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            LoadInformation();
+
+            _fontLoader = new EmbeddedFontLoader();
+
+            _fontLoader.LoadFontsFromResources(
+                "RED_X_LOGIN_UI.NevanRUS.ttf",
+                "RED_X_LOGIN_UI.NeutraTextTF-Demi.ttf"
+
+            );
+
+
+            label1.Font = new Font(_fontLoader.GetFontFamilyByName("Neutra Text TF Light"), 11f, FontStyle.Regular);
+            passwordBox.Font = new Font(_fontLoader.GetFontFamilyByName("Neutra Text TF Light"), 11f, FontStyle.Regular);
+            usernameBox.Font = new Font(_fontLoader.GetFontFamilyByName("Neutra Text TF Light"), 11f, FontStyle.Regular);
+            loginBtn.Font = new Font(_fontLoader.GetFontFamilyByName("Neutra Text TF Light"), 11f, FontStyle.Bold);
+
+
+
+
+
+            // Safely get the FontFamily
+            var fontFamily = _fontLoader.GetFontFamilyByName("Nevan RUS");
+
+            using (Graphics g = this.CreateGraphics())
+            {
+                Font font = new Font(fontFamily, 28, FontStyle.Bold);
+                SizeF textSize = g.MeasureString("RED-X LOGIN UI", font, Point.Empty, StringFormat.GenericTypographic);
+
+                startPosition = new PointF((this.ClientSize.Width - textSize.Width) / 2,
+                                           (this.ClientSize.Height - textSize.Height) / 2);
+                targetPosition = new PointF((this.ClientSize.Width - textSize.Width) / 2, 60);
+                currentPosition = startPosition;
+            }
+        }
+
+        // === Particle Update ===
+        private void UpdateParticles()
+        {
+            // Add new particles
+            if (particles.Count < 50) // limit for performance
+            {
+                particles.Add(new Particle
+                {
+                    Position = new PointF(rand.Next(0, this.Width), rand.Next(0, this.Height)),
+                    Velocity = new PointF((float)(rand.NextDouble() - 0.5f) * 2f, (float)(rand.NextDouble() - 0.5f) * 2f),
+                    Size = rand.Next(2, 5),
+                    Life = 255,
+                    Color = Color.FromArgb(200, Color.Red)
+                });
+            }
+
+            // Update existing
+            foreach (var p in particles.ToList())
+            {
+                p.Position = new PointF(p.Position.X + p.Velocity.X, p.Position.Y + p.Velocity.Y);
+                p.Life -= 2f;
+
+                if (p.Life <= 0)
+                    particles.Remove(p);
+            }
+        }
+
+
+        //PAINT OVERRRIDE
+        // === Draw Particles ===
+
+        private void LoadInformation()
+        {
+            usernameBox.Text = Properties.Settings.Default.usernameBox;
+            passwordBox.Text = Properties.Settings.Default.passwordBox;
+        }
+
+        private void SaveUserAndPass()
+        {
+            Properties.Settings.Default.usernameBox = usernameBox.Text;
+            Properties.Settings.Default.passwordBox = passwordBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            SaveUserAndPass();
+            // Input validation
+            if (string.IsNullOrWhiteSpace(usernameBox.Text))
+            {
+                label1.Text = "Please enter your username first";
+                return;
+            }
+
+
+
+            // Perform login
+            LicenseAuthApp.login(usernameBox.Text, passwordBox.Text);
+
+            if (LicenseAuthApp.response.success)
+            {
+                // Login success - open Main form and hide login
+                MainForm main = new MainForm();
+                main.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Login failed - show error message
+                label1.Text = "Status: " + LicenseAuthApp.response.message;
+            }
+        }
+
+        private void usernameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
